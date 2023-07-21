a5bf71008ea49d0464c2d4a04ce73ab501dfcf66
Taranvirs88 <113557821+Taranvirs88@users.noreply.github.com>
2023-07-21 19:01:10 -0400

    Created services

--- Tar_Atwal_MyFavouriteSingers/src/app/app.component.ts
+++ Tar_Atwal_MyFavouriteSingers/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SingerService } from './services/singer.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tar_Atwal_MyFavouriteSingers';
+  singleContentItem:any;
+  requestedContentId: any;
+  constructor(private singerService:SingerService){
+
+  }
+  ngOnInit(): void {
+    const idNumber = 3;
+    this.singerService.getContentItemById(idNumber).subscribe((data)=>{
+        this.singleContentItem = data;
+        console.log('singleContentItem', this.singleContentItem);
+    });
+  }
+
+  getRequestedContent() {
+    console.log('this.requestedContentId',this.requestedContentId);
+    this.singerService.getContentItemById(this.requestedContentId).subscribe(item => {
+      console.log('item',item);
+      this.singleContentItem = item;
+      console.log('singleContentItem click',this.singleContentItem);
+    });
+  }
 }
--- Tar_Atwal_MyFavouriteSingers/src/app/content-list/content-list.component.ts
+++ Tar_Atwal_MyFavouriteSingers/src/app/content-list/content-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { SingerService } from '../services/singer.service';
+import { contentList } from '../helper-files/contentDb';
 
 @Component({
   selector: 'app-content-list',
@@ -7,71 +9,8 @@ import { Content } from '../helper-files/content-interface';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
-    contentList:Content[] =[
-      {
-        id: 1,
-        title: 'Tera Tera',
-        imgURL: 'assets/img/img1.jpg',
-        description: 'Tarsem Jassar has written and sung the song Tera Tera that emphasizes how we humans like to claim rights to everything we come across in the world; while none of it belongs to us.',
-        creator: 'Tarsem Jassar',
-        type: '',
-        tags: ['2019']
-      },
-      {
-        id: 2,
-        title: 'Leekan',
-        imgURL: '',
-        description: 'Leekan Song from the Ashke album is voiced by famous singer Amrinder Gill. The lyrics of Leekan Song from Ashke album are written by Raj Ranjodh',
-        creator: 'Amrinder Gill',
-         type: 'Romantic',
-        tags: ['2018']
-      },
-      {
-        id: 3,
-        title: 'Rutba',
-        imgURL: 'assets/img/Img3.jpg',
-        description: 'Rutba Lyrics by Satinder Sartaaj is brand new Punjabi song from movie Kali Jotta and this latest song is featuring Neeru Bajwa, Wamiqa Gabbi.',
-        creator: 'Satinder Sartaj ',
-        type: 'Romantic',
-        tags: ['2023']
-      },
-      {
-        id: 4,
-        title: 'Laung Laachi',
-        imgURL: 'assets/img/img4.png',
-        description: 'Laung Laachi is a Punjabi romantic song from the movie "Laung Laachi".',
-        creator: 'Mannat Noor',
-        type: 'Punjabi',
-        tags: ['Romantic', 'Punjabi']
-      },
-      {
-        id: 5,
-        title: 'Lamberghini',
-        imgURL: 'assets/img/img5.jpg',
-        description: 'Lamberghini is a popular Punjabi song by The Doorbeen featuring Ragini.',
-        creator: 'Jassi Gill',
-        type: 'Punjabi',
-        tags: ['Pop', 'Punjabi']
-      },
-      {
-        id: 6,
-        title: 'Lamberghini',
-        imgURL: 'assets/img/img5.jpg',
-        description: 'Lamberghini is a popular Punjabi song by The Doorbeen featuring Ragini.',
-        creator: 'Jassi Gill',
-        type: '',
-        tags: ['Pop', 'Punjabi']
-      },
-      {
-        id: 7,
-        title: 'Lamberghini02',
-        imgURL: 'assets/img/img5.jpg',
-        description: 'Lamberghini is a popular Punjabi song by The Doorbeen featuring Ragini.',
-        creator: 'Jassi Gill',
-        type: 'Punjabi',
-        tags: ['Pop', 'Punjabi']
-      },
-    ];
+    contentList:any[];
+     
     findTitle : string ='';
     filterResult: boolean = false;
     message :string =''
